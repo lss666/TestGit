8eed6196e829474ed303ce186fe395bfb8733487
like1breath <951955109@qq.com>
2017-11-16 15:57:33 +0800

    first

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,11 @@
+/**
+ * TestGit的类
+ *
+ * @author lss
+ * @create 2017-11-16 15:49
+ */
+public class TestGit {
+    public static void main(String[] args){
+        System.out.println("hehe");
+    }
+}
